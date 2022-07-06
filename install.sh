a206e192580716135ccfe502b9b0e53c6c5d2dd2
Howardnm <55622355+Howardnm@users.noreply.github.com>
2022-07-07 01:16:23 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ rm -f /root/myddns/*
 wget -P /root/myddns/ https://cdn.jsdelivr.net/gh/Howardnm/dnspod-shell@old/ardnspod
 wget -P /root/myddns/ https://cdn.jsdelivr.net/gh/Howardnm/dnspod-shell@old/ddnspod.sh
 wget -P /root/myddns/ https://cdn.jsdelivr.net/gh/Howardnm/dnspod-shell@old/myddns.sh
+echo "安装完成"
 rm -- "$0"
