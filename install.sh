2d7631f3a3e93ca0dd6995e2f09611f1a1705f23
Howardnm <55622355+Howardnm@users.noreply.github.com>
2022-07-07 01:07:22 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 #
 mkdir /root/myddns
 rm -f /root/myddns/*
-wget -P /root/myddns/ https://ghproxy.com/https://raw.githubusercontent.com/Howardnm/dnspod-shell/old/ardnspod
-wget -P /root/myddns/ https://ghproxy.com/https://raw.githubusercontent.com/Howardnm/dnspod-shell/old/ddnspod.sh
-wget -P /root/myddns/ https://ghproxy.com/https://raw.githubusercontent.com/Howardnm/dnspod-shell/old/myddns.sh
+wget -P /root/myddns/ https://cdn.jsdelivr.net/gh/Howardnm/dnspod-shell@old/ardnspod
+wget -P /root/myddns/ https://cdn.jsdelivr.net/gh/Howardnm/dnspod-shell@old/ddnspod.sh
+wget -P /root/myddns/ https://cdn.jsdelivr.net/gh/Howardnm/dnspod-shell@old/myddns.sh
 rm -- "$0"
